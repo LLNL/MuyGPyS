13f24f79e641afa75101060c319f3767a6d0b8c4
Ben Priest <bwpriester@gmail.com>
2022-09-15 15:39:29 -0700

    Added utility function to consistently reduce a scalar only when in mpi mode

--- MuyGPyS/_src/mpi_utils.py
+++ MuyGPyS/_src/mpi_utils.py
@@ -5,6 +5,8 @@
 
 from MuyGPyS import config
 
+from mpi4py import MPI
+
 from typing import Callable
 
 import numpy as np
@@ -94,6 +96,13 @@ def _consistent_chunk_tensor(tensor) -> np.ndarray:
         return _chunk_tensor(tensor)
     else:
         return tensor
+
+
+def _consistent_reduce_scalar(scalar):
+    if _is_mpi_mode() is True:
+        return world.allreduce(scalar, op=MPI.SUM)
+    else:
+        return scalar
 
 
 def _is_mpi_mode() -> bool:
