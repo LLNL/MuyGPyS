f13017163c9055feb7843f81cd7818b74f398e2d
Alec Dunton <dunton1@llnl.gov>
2022-11-02 11:27:47 -0700

    Workflow is working - need to add documentation

--- MuyGPyS/examples/fast_regress.py
+++ MuyGPyS/examples/fast_regress.py
@@ -22,7 +22,10 @@ import numpy as np
 from time import perf_counter
 from typing import Dict, List, Optional, Tuple, Union
 
-from MuyGPyS.gp.distance import make_train_tensors
+from MuyGPyS.gp.distance import (
+    make_train_tensors,
+    crosswise_distances,
+)
 from MuyGPyS.optimize.chassis import optimize_from_tensors
 
 from MuyGPyS.gp.muygps import MuyGPS, MultivariateMuyGPS as MMuyGPS
@@ -47,16 +50,17 @@ def make_fast_regressor(
     nbrs_lookup: NN_Wrapper,
     train_features: np.ndarray,
     train_responses: np.ndarray,
-) -> np.ndarray:
+) -> Tuple[np.ndarray, np.ndarray]:
     num_training_samples, _ = train_features.shape
     nn_indices, _ = nbrs_lookup.get_batch_nns(
         np.arange(0, num_training_samples)
     )
     nn_indices = np.array(nn_indices).astype(int)
+
     precomputed_coefficients_matrix = muygps.build_fast_regress_coeffs(
         train_features, nn_indices, train_responses
     )
-    return precomputed_coefficients_matrix
+    return precomputed_coefficients_matrix, nn_indices
 
 
 def make_fast_multivariate_regressor(
@@ -64,51 +68,86 @@ def make_fast_multivariate_regressor(
     nbrs_lookup: NN_Wrapper,
     train_features: np.ndarray,
     train_responses: np.ndarray,
-) -> np.ndarray:
+) -> Tuple[np.ndarray, np.ndarray]:
     num_training_samples, _ = train_features.shape
     nn_indices, _ = nbrs_lookup.get_batch_nns(
         np.arange(0, num_training_samples)
     )
-    nn_indices = np.array(nn_indices).astype(int)
+
     precomputed_coefficients_matrix = muygps.build_fast_regress_coeffs(
         train_features, nn_indices, train_responses
     )
-    return precomputed_coefficients_matrix
+    return precomputed_coefficients_matrix, nn_indices
 
 
 # choose between multivariate and univariate
 def _decide_and_make_fast_regressor(
-    muygps: MMuyGPS,
+    muygps: Union[MuyGPS, MMuyGPS],
     nbrs_lookup: NN_Wrapper,
     train_features: np.ndarray,
     train_responses: np.ndarray,
 ) -> np.ndarray:
     if isinstance(muygps, MuyGPS):
-        return make_fast_regressor(
-            muygps, nbrs_lookup, train_features, train_responses
+        precomputed_coefficients_matrix, nn_indices = make_fast_regressor(
+            muygps,
+            nbrs_lookup,
+            train_features,
+            train_responses,
         )
     else:
-        return make_fast_multivariate_regressor(
-            muygps, nbrs_lookup, train_features, train_responses
+        (
+            precomputed_coefficients_matrix,
+            nn_indices,
+        ) = make_fast_multivariate_regressor(
+            muygps,
+            nbrs_lookup,
+            train_features,
+            train_responses,
         )
-
-
-def _unpack():
-    return
+    return precomputed_coefficients_matrix, nn_indices
 
 
 # workflow, return relevant structures
 def do_fast_regress(
-    muygps: MMuyGPS,
+    muygps: Union[MuyGPS, MMuyGPS],
     nbrs_lookup: NN_Wrapper,
     train_features: np.ndarray,
     train_responses: np.ndarray,
     test_features: np.ndarray,
 ) -> np.ndarray:
 
-    return
+    (
+        precomputed_coefficients_matrix,
+        nn_indices,
+    ) = _decide_and_make_fast_regressor(
+        muygps,
+        nbrs_lookup,
+        train_features,
+        train_responses,
+    )
+    num_training_samples, _ = train_features.shape
+    _, nn_count = nn_indices.shape
+    nn_indices_with_self = np.zeros((num_training_samples, nn_count + 1))
+    nn_indices_with_self[:, 1 : nn_count + 1] = nn_indices
+    nn_indices_with_self[:, 0] = np.arange(0, num_training_samples)
+    nn_indices = nn_indices_with_self[:, :-1].astype(int)
 
+    test_neighbors, _ = nbrs_lookup.get_nns(test_features)
+    closest_neighbor = test_neighbors[:, 0]
+    closest_set_new = nn_indices[closest_neighbor, :].astype(int)
+    num_test_samples, _ = test_features.shape
 
-# fast regression with timing
-def fast_regress_any():
-    return
+    crosswise_dist_tens = crosswise_distances(
+        test_features,
+        train_features,
+        np.arange(0, num_test_samples),
+        closest_set_new,
+    )
+    Kcross_test_tens = muygps.kernel(crosswise_dist_tens)
+    predictions = muygps.fast_regress_from_indices(
+        Kcross_test_tens,
+        closest_neighbor,
+        precomputed_coefficients_matrix,
+    )
+
+    return predictions
