6268d33256d23c179b68e2412e5ee61755ec7ac3
Alec Dunton <dunton1@llnl.gov>
2022-11-02 12:27:10 -0700

    Added end-to-end workflow function

--- MuyGPyS/examples/fast_regress.py
+++ MuyGPyS/examples/fast_regress.py
@@ -41,6 +41,8 @@ from MuyGPyS.examples.regress import (
     make_multivariate_regressor,
     _empirical_correlation,
     _empirical_covariance,
+    _decide_and_make_regressor,
+    _unpack,
 )
 
 
@@ -107,13 +109,217 @@ def _decide_and_make_fast_regressor(
     return precomputed_coefficients_matrix, nn_indices
 
 
-# workflow, return relevant structures
 def do_fast_regress(
-    muygps: Union[MuyGPS, MMuyGPS],
-    nbrs_lookup: NN_Wrapper,
-    train_features: np.ndarray,
-    train_responses: np.ndarray,
     test_features: np.ndarray,
+    train_features: np.ndarray,
+    train_targets: np.ndarray,
+    nn_count: int = 30,
+    batch_count: int = 200,
+    loss_method: str = "lool",
+    obj_method: str = "loo_crossval",
+    opt_method: str = "bayes",
+    sigma_method: Optional[str] = "analytic",
+    kern: Optional[str] = None,
+    k_kwargs: Union[Dict, Union[List[Dict], Tuple[Dict, ...]]] = dict(),
+    nn_kwargs: Dict = dict(),
+    opt_kwargs: Dict = dict(),
+    apply_sigma_sq: bool = True,
+    return_distances: bool = False,
+    verbose: bool = False,
+) -> np.ndarray:
+    """
+    Convenience function initializing a model and performing regression.
+
+    Expected parameters include keyword argument dicts specifying kernel
+    parameters and nearest neighbor parameters. See the docstrings of the
+    appropriate functions for specifics.
+
+    Also supports workflows relying upon multivariate models. In order to create
+    a multivariate model, specify the `kern` argument and pass a list of
+    hyperparameter dicts to `k_kwargs`.
+
+    Example:
+        >>> from MuyGPyS.testing.test_utils import _make_gaussian_data
+        >>> from MuyGPyS.examples.regress import do_regress
+        >>> from MuyGPyS.optimize.objective import mse_fn
+        >>> train, test = _make_gaussian_data(10000, 1000, 100, 10)
+        >>> nn_kwargs = {"nn_method": "exact", "algorithm": "ball_tree"}
+        >>> k_kwargs = {
+        ...         "kern": "rbf",
+        ...         "metric": "F2",
+        ...         "eps": {"val": 1e-5},
+        ...         "length_scale": {"val": 1.0, "bounds": (1e-2, 1e2)}
+        ... }
+        >>> muygps, nbrs_lookup, predictions, variance = do_regress(
+        ...         test['input'],
+        ...         train['input'],
+        ...         train['output'],
+        ...         nn_count=30,
+        ...         batch_count=200,
+        ...         loss_method="mse",
+        ...         obj_method="loo_crossval",
+        ...         opt_method="bayes",
+        ...         variance_mode="diagonal",
+        ...         k_kwargs=k_kwargs,
+        ...         nn_kwargs=nn_kwargs,
+        ...         verbose=False,
+        ... )
+        >>> # Can alternately return distance tensors for reuse
+        >>> muygps, nbrs_lookup, predictions, variance, crosswise_dists, pairwise_dists = do_regress(
+        ...         test['input'],
+        ...         train['input'],
+        ...         train['output'],
+        ...         nn_count=30,
+        ...         batch_count=200,
+        ...         loss_method="mse",
+        ...         obj_method="loo_crossval",
+        ...         opt_method="bayes",
+        ...         variance_mode="diagonal",
+        ...         k_kwargs=k_kwargs,
+        ...         nn_kwargs=nn_kwargs,
+        ...         return_distances=True,
+        ...         verbose=False,
+        ... )
+        >>> mse = mse_fn(test['output'], predictions)
+        >>> print(f"obtained mse: {mse}")
+        obtained mse: 0.20842...
+
+    Args:
+        test_features:
+            A matrix of shape `(test_count, feature_count)` whose rows consist
+            of observation vectors of the test data.
+        train_features:
+            A matrix of shape `(train_count, feature_count)` whose rows consist
+            of observation vectors of the train data.
+        train_targets:
+            A matrix of shape `(train_count, response_count)` whose rows consist
+            of response vectors of the train data.
+        nn_count:
+            The number of nearest neighbors to employ.
+        batch_count:
+            The number of elements to sample batch for hyperparameter
+            optimization.
+        loss_method:
+            The loss method to use in hyperparameter optimization. Ignored if
+            all of the parameters specified by argument `k_kwargs` are fixed.
+            Currently supports only `"mse"` for regression.
+        obj_method:
+            Indicates the objective function to be minimized. Currently
+            restricted to `"loo_crossval"`.
+        opt_method:
+            Indicates the optimization method to be used. Currently restricted
+            to `"bayesian"` and `"scipy"`.
+        sigma_method:
+            The optimization method to be employed to learn the `sigma_sq`
+            hyperparameter. Currently supports only `"analytic"` and `None`. If
+            the value is not `None`, the returned
+            :class:`MuyGPyS.gp.muygps.MuyGPS` object will possess a `sigma_sq`
+            member whose value, invoked via `muygps.sigma_sq()`, is a
+            `(response_count,)` vector to be used for scaling posterior
+            variances.
+        variance_mode:
+            Specifies the type of variance to return. Currently supports
+            `diagonal` and None. If None, report no variance term.
+        kern:
+            The kernel function to be used. See :ref:`MuyGPyS-gp-kernels` for
+            details. Only used in the multivariate case. If `None`, assume
+            that we are not using a multivariate model.
+        k_kwargs:
+            If given a list or tuple of length `response_count`, assume that the
+            elements are dicts containing kernel initialization keyword
+            arguments for the creation of a multivariate model (see
+            :func:`~MuyGPyS.examples.regress.make_multivariate_regressor`).
+            If given a dict, assume that the elements are keyword arguments to
+            a MuyGPs model (see
+            :func:`~MuyGPyS.examples.regress.make_regressor`).
+        nn_kwargs:
+            Parameters for the nearest neighbors wrapper. See
+            :class:`MuyGPyS.neighbors.NN_Wrapper` for the supported methods and
+            their parameters.
+        opt_kwargs:
+            Parameters for the wrapped optimizer. See the docs of the
+            corresponding library for supported parameters.
+        apply_sigma_sq:
+            If `True` and `variance_mode is not None`, automatically scale the
+            posterior variances by `sigma_sq`.
+        return_distances:
+            If `True`, returns a `(test_count, nn_count)` matrix containing the
+            crosswise distances between the test elements and their nearest
+            neighbor sets and a `(test_count, nn_count, nn_count)` tensor
+            containing the pairwise distances between the test's nearest
+            neighbor sets.
+        verbose:
+            If `True`, print summary statistics.
+
+    Returns
+    -------
+    muygps:
+        A (possibly trained) MuyGPs object.
+    nbrs_lookup:
+        A data structure supporting nearest neighbor queries into
+        `train_features`.
+    predictions:
+        The predicted response associated with each test observation.
+    variance:
+        Estimated posterior variance of each test prediction. If
+        `variance_mode == "diagonal"` return a `(test_count, response_count)`
+        matrix where each row is the posterior variance. If
+        `sigma_method is not None` and `apply_sigma_sq is True`, each column
+        of the variance is automatically scaled by the corresponding `sigma_sq`
+        parameter.
+    crosswise_dists:
+        A matrix of shape `(test_count, nn_count)` whose rows list the distance
+        of the corresponding test element to each of its nearest neighbors.
+        Only returned if `return_distances is True`.
+    pairwise_dists:
+        A tensor of shape `(test_count, nn_count, nn_count,)` whose latter two
+        dimensions contain square matrices containing the pairwise distances
+        between the nearest neighbors of the test elements. Only returned if
+        `return_distances is True`.
+    """
+    if sigma_method is None:
+        apply_sigma_sq = False
+
+    regressor_args = _decide_and_make_regressor(
+        train_features,
+        train_targets,
+        nn_count=nn_count,
+        batch_count=batch_count,
+        loss_method=loss_method,
+        obj_method=obj_method,
+        opt_method=opt_method,
+        sigma_method=sigma_method,
+        kern=kern,
+        k_kwargs=k_kwargs,
+        nn_kwargs=nn_kwargs,
+        opt_kwargs=opt_kwargs,
+        return_distances=False,
+        verbose=verbose,
+    )
+    regressor, regressor_args_less1 = _unpack(*regressor_args)
+    nbrs_lookup, regressor_args_less2 = _unpack(*regressor_args_less1)
+    if len(regressor_args_less2) > 0:
+        # Should not get here
+        # crosswise_dists, pairwise_dists = regressor_args_less2
+        pass
+
+    predictions = fast_regress_any(
+        regressor,
+        test_features,
+        train_features,
+        nbrs_lookup,
+        train_targets,
+    )
+    return regressor, nbrs_lookup, predictions
+
+
+# workflow, return relevant structures
+def fast_regress_any(
+    muygps: Union[MuyGPS, MMuyGPS],
+    test_features: np.ndarray,
+    train_features: np.ndarray,
+    nbrs_lookup: NN_Wrapper,
+    train_targets: np.ndarray,
 ) -> np.ndarray:
 
     (
@@ -123,7 +329,7 @@ def do_fast_regress(
         muygps,
         nbrs_lookup,
         train_features,
-        train_responses,
+        train_targets,
     )
     num_training_samples, _ = train_features.shape
     _, nn_count = nn_indices.shape
