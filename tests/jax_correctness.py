1bd59179e7fff669d4c7c41ab7d1580bb7354b4b
Alec Dunton <dunton1@llnl.gov>
2022-11-08 11:48:42 -0800

    Debugging Jax Tests

--- tests/jax_correctness.py
+++ tests/jax_correctness.py
@@ -545,9 +545,8 @@ if config.muygpys_jax_enabled is True:  # type: ignore
             cls.nn_indices_with_self_j = jnp.zeros(
                 (cls.train_count, cls.nn_count + 1)
             )
-            cls.new_nn_indices_j = jnp.concatenate((
-                jnp.arange(0, cls.train_count),
-                cls.nn_indices_all_j[:, -1],)
+            cls.new_nn_indices_j = jnp.concatenate(
+                (jnp.arange(0, cls.train_count), cls.nn_indices_all_j[:, -1]),
                 axis=1,
             )
             cls.closest_set_new_j = cls.new_nn_indices_j[
