3f70f76e3ec1a1d17e46adec7033bd896d2a6bac
Alec Dunton <dunton1@llnl.gov>
2022-11-09 15:49:36 -0800

    Added Star Galaxy API Test Case

--- tests/api.py
+++ tests/api.py
@@ -674,6 +674,105 @@ class HeatonTest(FastRegressionAPITest):
         )
 
 
+class MultivariateStargalFastRegressTest(FastRegressionAPITest):
+    @classmethod
+    def setUpClass(cls):
+        super(MultivariateStargalFastRegressTest, cls).setUpClass()
+        with open(
+            os.path.join(hardpath + stargal_dir, stargal_files["40"]), "rb"
+        ) as f:
+            cls.embedded_40_train, cls.embedded_40_test = pkl.load(f)
+        # with open(os.path.join(hardpath, heaton_file), "rb") as f:
+        #     cls.train, cls.test = pkl.load(f)
+
+    @parameterized.parameters(
+        (
+            (nn, bs, vm, lm, om, opt_method_and_kwargs, nn_kwargs, k_kwargs)
+            for nn in [30]
+            for bs in [500]
+            for vm in [None, "diagonal"]
+            for lm in ["mse"]
+            for om in ["loo_crossval"]
+            for nn_kwargs in _basic_nn_kwarg_options
+            for opt_method_and_kwargs in _basic_opt_method_and_kwarg_options
+            # for vm in ["diagonal"]
+            # for nn_kwargs in [_basic_nn_kwarg_options[0]]
+            # for opt_method_and_kwargs in [
+            #     _basic_opt_method_and_kwarg_options[0]
+            # ]
+            for k_kwargs in (
+                (
+                    1.0,
+                    "matern",
+                    [
+                        {
+                            "nu": {"val": "sample", "bounds": (1e-1, 1e0)},
+                            # "nu": {"val": 0.38},
+                            "length_scale": {"val": 1.5},
+                            "eps": {"val": 1e-3},
+                        },
+                        {
+                            "nu": {"val": 0.5},
+                            # "nu": {"val": 0.38},
+                            "length_scale": {"val": 1.5},
+                            "eps": {"val": 1e-3},
+                        },
+                    ],
+                ),
+                (
+                    1.0,
+                    "rbf",
+                    [
+                        {"length_scale": {"val": 1.5}, "eps": {"val": 1e-3}},
+                        {"length_scale": {"val": 1.5}, "eps": {"val": 1e-3}},
+                    ],
+                ),
+            )
+        )
+    )
+    def test_fast_regress(
+        self,
+        nn_count,
+        batch_count,
+        variance_mode,
+        loss_method,
+        obj_method,
+        opt_method_and_kwargs,
+        nn_kwargs,
+        k_kwargs,
+    ):
+        target_mse, kern, k_args = k_kwargs
+        opt_method, opt_kwargs = opt_method_and_kwargs
+        train = _balanced_subsample(self.embedded_40_train, 10000)
+        test = _balanced_subsample(self.embedded_40_test, 1000)
+
+        if variance_mode is None:
+            sigma_method = None
+            apply_sigma_sq = False
+        else:
+            sigma_method = "analytic"
+            apply_sigma_sq = True
+
+        self._do_fast_regress_test_chassis(
+            train=train,
+            test=test,
+            target_mse=target_mse,
+            nn_count=nn_count,
+            batch_count=batch_count,
+            loss_method=loss_method,
+            obj_method=obj_method,
+            opt_method=opt_method,
+            sigma_method=sigma_method,
+            variance_mode=variance_mode,
+            nn_kwargs=nn_kwargs,
+            kern=kern,
+            k_kwargs=k_args,
+            opt_kwargs=opt_kwargs,
+            apply_sigma_sq=apply_sigma_sq,
+            verbose=False,
+        )
+
+
 if __name__ == "__main__":
     if os.path.isdir(sys.argv[-1]):
         hardpath = sys.argv[-1]
