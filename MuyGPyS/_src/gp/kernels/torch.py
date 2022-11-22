d0a0763614ff12ebae4367d10cd8461e0db6d634
Alec Dunton <dunton1@llnl.gov>
2022-11-22 10:46:29 -0800

    added kernel functions in torch

---
+++ MuyGPyS/_src/gp/kernels/torch.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def _rbf_fn(squared_dists: torch.Tensor, length_scale: float) -> torch.Tensor:
+    return torch.exp(-squared_dists / (2 * length_scale**2))
+
+
+def _matern_05_fn(dists: torch.Tensor, length_scale: float) -> torch.Tensor:
+    dists = dists / length_scale
+    return torch.exp(-dists)
+
+
+def _matern_15_fn(dists: torch.Tensor, length_scale: float) -> torch.Tensor:
+    dists = dists / length_scale
+    K = dists * torch.sqrt(torch.tensor(3))
+    return (1.0 + K) * torch.exp(-K)
+
+
+def _matern_25_fn(dists: torch.Tensor, length_scale: float) -> torch.Tensor:
+    dists = dists / length_scale
+    K = dists * torch.sqrt(torch.tensor(5))
+    return (1.0 + K + K**2 / 3.0) * torch.exp(-K)
+
+
+def _matern_inf_fn(dists: torch.Tensor, length_scale: float) -> torch.Tensor:
+    dists = dists / length_scale
+    return torch.exp(-(dists**2) / 2.0)
