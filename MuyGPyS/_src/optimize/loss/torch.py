df9cfc8fe62e8fff415ce66503b373fa97c5191a
Alec Dunton <dunton1@llnl.gov>
2022-11-22 11:16:45 -0800

    added _src.optoimize.loss functions in torch

---
+++ MuyGPyS/_src/optimize/loss/torch.py
@@ -0,0 +1,44 @@
+import torch
+
+
+def _cross_entropy_fn(
+    predictions: torch.Tensor,
+    targets: torch.Tensor,
+    ll_eps: float = 1e-15,
+) -> float:
+    one_hot_targets = torch.where(targets > 0.0, 1.0, 0.0)
+    softmax_predictions = torch.nn.Softmax(predictions, axis=1)
+
+    return torch.nn.CrossEntropyLoss(
+        one_hot_targets, softmax_predictions, eps=ll_eps, normalize=False
+    )
+
+
+def _mse_fn_unnormalized(
+    predictions: torch.Tensor,
+    targets: torch.Tensor,
+) -> float:
+    return torch.sum((predictions - targets) ** 2)
+
+
+def _mse_fn(
+    predictions: torch.Tensor,
+    targets: torch.Tensor,
+) -> float:
+    batch_count, response_count = predictions.shape
+    return _mse_fn_unnormalized(predictions, targets) / (
+        batch_count * response_count
+    )
+
+
+def _lool_fn(
+    predictions: torch.Tensor,
+    targets: torch.Tensor,
+    variances: torch.Tensor,
+    sigma_sq: torch.Tensor,
+) -> float:
+    scaled_variances = torch.outer(variances, sigma_sq)
+    return torch.sum(
+        torch.divide((predictions - targets) ** 2, scaled_variances)
+        + torch.log(scaled_variances)
+    )
