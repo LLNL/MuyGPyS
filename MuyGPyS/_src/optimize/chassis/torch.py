7831f4be61ffdda7798eab13f3e70ba9d2a89dc6
Alec Dunton <dunton1@llnl.gov>
2022-12-08 14:45:56 -0500

    Added not implemented exception functions to optimize chassis

---
+++ MuyGPyS/_src/optimize/chassis/torch.py
@@ -0,0 +1,31 @@
+# Copyright 2021-2022 Lawrence Livermore National Security, LLC and other
+# MuyGPyS Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from copy import deepcopy
+from typing import Callable
+
+from MuyGPyS.gp.muygps import MuyGPS
+
+
+def _scipy_optimize(
+    muygps: MuyGPS,
+    obj_fn: Callable,
+    verbose: bool = False,
+    **kwargs,
+) -> MuyGPS:
+    raise NotImplementedError(
+        f"Scipy optimization is not supported in MuyGPs PyTorch module."
+    )
+
+
+def _bayes_opt_optimize(
+    muygps: MuyGPS,
+    obj_fn: Callable,
+    verbose: bool = False,
+    **kwargs,
+) -> MuyGPS:
+    raise NotImplementedError(
+        f"Bayesian optimization is not supported in MuyGPs PyTorch module."
+    )
