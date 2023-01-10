dbb1852fefd2167c37c6a5573634c8dbad395db6
Benjamin Priest (they/them) <bwpriester@gmail.com>
2023-01-09 18:32:07 -0800

    Updated copyright dates (#82)

--- scripts/lc-setup/pascal.sh
+++ scripts/lc-setup/pascal.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2021-2022 Lawrence Livermore National Security, LLC and other
+# Copyright 2021-2023 Lawrence Livermore National Security, LLC and other
 # MuyGPyS Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: MIT
