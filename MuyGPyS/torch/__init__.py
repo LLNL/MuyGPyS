5ffb5d9127b7a3d9b4275441c2da1beb8c8b094e
Benjamin Priest (they/them) <bwpriester@gmail.com>
2023-02-01 10:27:28 -0800

    Added missing __init__.py to MuyGPyS.torch package (#89)

---
+++ MuyGPyS/torch/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021-2023 Lawrence Livermore National Security, LLC and other
+# MuyGPyS Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
