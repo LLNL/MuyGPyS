9477cc457fd3921942705ced043a31ba0836de72
Alec Dunton <dunton1@llnl.gov>
2023-04-26 14:25:39 -0700

    Unused import error

--- tests/muygps_torch/muygps_torch.py
+++ tests/muygps_torch/muygps_torch.py
@@ -9,9 +9,8 @@ from absl.testing import parameterized
 import MuyGPyS._src.math.numpy as np
 import MuyGPyS._src.math.torch as torch
 from MuyGPyS import config
-from MuyGPyS._test.torch_utils import SVDKMuyGPs, SVDKMultivariateMuyGPs
+from MuyGPyS._test.torch_utils import SVDKMultivariateMuyGPs
 from MuyGPyS._test.utils import _check_ndarray, _make_gaussian_data
-from MuyGPyS.gp.muygps import MuyGPS
 from MuyGPyS.gp.multivariate_muygps import MultivariateMuyGPS as MMuyGPS
 from MuyGPyS.gp.kernels import Matern
 from MuyGPyS.gp.distortion import IsotropicDistortion
