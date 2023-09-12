f4fdd80d8a335a44363cdcceec7d39411910fe24
Benjamin W. Priest (they/them) <bwpriester@gmail.com>
2023-09-12 12:28:06 -0700

    improved shear kernel runtime by a factor of ~2 by precomputing all of the difference tensors used across subkernels.

--- MuyGPyS/_test/shear.py
+++ MuyGPyS/_test/shear.py
@@ -8,11 +8,7 @@ from typing import Union
 import MuyGPyS._src.math.numpy as np
 
 
-def kk_f(diffs, a=1, length_scale=1):
-    sq_diffs = diffs**2
-    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
-    prod_sq_diffs = np.prod(sq_diffs, axis=-1)
-    sum_quad_diffs = np.sum(sq_diffs**2, axis=-1)
+def kk_f(sum_sq_diffs, prod_sq_diffs, sum_quad_diffs, a=1, length_scale=1):
     return (
         1
         / 4
@@ -30,12 +26,13 @@ def kk_f(diffs, a=1, length_scale=1):
     )
 
 
-def kg1_f(diffs, a=1, length_scale=1):
-    sq_diffs = diffs**2
-    quad_diffs = sq_diffs**2
-    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
-    diff_xy_quad_diffs = quad_diffs[..., 0] - quad_diffs[..., 1]
-    diff_yx_sq_diffs = sq_diffs[..., 1] - sq_diffs[..., 0]
+def kg1_f(
+    sum_sq_diffs,
+    diff_xy_quad_diffs,
+    diff_yx_sq_diffs,
+    a=1,
+    length_scale=1,
+):
     return (
         1
         / 4
@@ -48,10 +45,7 @@ def kg1_f(diffs, a=1, length_scale=1):
     )
 
 
-def kg2_f(diffs, a=1, length_scale=1):
-    sq_diffs = diffs**2
-    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
-    prod_diffs = np.prod(diffs, axis=-1)
+def kg2_f(sum_sq_diffs, prod_diffs, a=1, length_scale=1):
     return (
         1
         / 4
@@ -66,11 +60,7 @@ def kg2_f(diffs, a=1, length_scale=1):
     )
 
 
-def g1g1_f(diffs, a=1, length_scale=1):
-    sq_diffs = diffs**2
-    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
-    sum_quad_diffs = np.sum(sq_diffs**2, axis=-1)
-    prod_sq_diffs = np.prod(sq_diffs, axis=-1)
+def g1g1_f(sum_sq_diffs, sum_quad_diffs, prod_sq_diffs, a=1, length_scale=1):
     return (
         1
         / 4
@@ -88,11 +78,7 @@ def g1g1_f(diffs, a=1, length_scale=1):
     )
 
 
-def g1g2_f(diffs, a=1, length_scale=1):
-    sq_diffs = diffs**2
-    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
-    diff_xy_sq_diffs = sq_diffs[..., 0] - sq_diffs[..., 1]
-    prod_diffs = np.prod(diffs, axis=-1)
+def g1g2_f(sum_sq_diffs, diff_xy_sq_diffs, prod_diffs, a=1, length_scale=1):
     return (
         1
         / 4
@@ -107,10 +93,7 @@ def g1g2_f(diffs, a=1, length_scale=1):
     )
 
 
-def g2g2_f(diffs, a=1, length_scale=1):
-    sq_diffs = diffs**2
-    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
-    prod_sq_diffs = np.prod(sq_diffs, axis=-1)
+def g2g2_f(sum_sq_diffs, prod_sq_diffs, a=1, length_scale=1):
     return (
         1
         / 4
@@ -130,17 +113,35 @@ def shear_kernel(diffs, a=1, length_scale=1):
     shape[-1] *= 3
     shape[-2] *= 3
     full_m = np.zeros(shape)
-    full_m[..., 0::3, 0::3] = kk_f(diffs, a, length_scale)
+
+    # compute intermediate difference tensors once here
+    prod_diffs = np.prod(diffs, axis=-1)
+    sq_diffs = diffs**2
+    quad_diffs = sq_diffs**2
+    sum_sq_diffs = np.sum(sq_diffs, axis=-1)
+    prod_sq_diffs = np.prod(sq_diffs, axis=-1)
+    sum_quad_diffs = np.sum(quad_diffs, axis=-1)
+    diff_yx_sq_diffs = sq_diffs[..., 1] - sq_diffs[..., 0]
+    diff_xy_sq_diffs = sq_diffs[..., 0] - sq_diffs[..., 1]
+    diff_xy_quad_diffs = quad_diffs[..., 0] - quad_diffs[..., 1]
+
+    full_m[..., 0::3, 0::3] = kk_f(
+        sum_sq_diffs, prod_sq_diffs, sum_quad_diffs, a, length_scale
+    )
     full_m[..., 0::3, 1::3] = full_m[..., 1::3, 0::3] = kg1_f(
-        diffs, a, length_scale
+        sum_sq_diffs, diff_xy_quad_diffs, diff_yx_sq_diffs, a, length_scale
     )
     full_m[..., 0::3, 2::3] = full_m[..., 2::3, 0::3] = kg2_f(
-        diffs, a, length_scale
+        sum_sq_diffs, prod_diffs, a, length_scale
     )
-    full_m[..., 1::3, 1::3] = g1g1_f(diffs, a, length_scale)
+    full_m[..., 1::3, 1::3] = g1g1_f(
+        sum_sq_diffs, sum_quad_diffs, prod_sq_diffs, a, length_scale
+    )
     full_m[..., 1::3, 2::3] = full_m[..., 2::3, 1::3] = g1g2_f(
-        diffs, a, length_scale
+        sum_sq_diffs, diff_xy_sq_diffs, prod_diffs, a, length_scale
     )
-    full_m[..., 2::3, 2::3] = g2g2_f(diffs, a, length_scale)
+    full_m[..., 2::3, 2::3] = g2g2_f(
+        sum_sq_diffs, prod_sq_diffs, a, length_scale
+    )
 
     return full_m
