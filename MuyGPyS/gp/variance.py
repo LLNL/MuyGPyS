4a1acf4c08de9c0027a96787e9cdcc0435b1a1d9
Min W. Priest (they/them) <bwpriester@gmail.com>
2023-12-18 15:19:38 -0800

    reorganized temporary reshaping in tensor math to make it easier to modify. removed unused apply_scale flag. removed insertion of initial scale parameter to optimization variance function.

--- MuyGPyS/gp/variance.py
+++ MuyGPyS/gp/variance.py
@@ -13,6 +13,7 @@ import MuyGPyS._src.math as mm
 from MuyGPyS._src.gp.muygps import _muygps_diagonal_variance
 from MuyGPyS.gp.hyperparameter import ScaleFn
 from MuyGPyS.gp.noise import NoiseFn
+from MuyGPyS.gp.mean import _reshaper_to_be_removed
 
 
 class PosteriorVariance:
@@ -20,13 +21,12 @@ class PosteriorVariance:
         self,
         noise: NoiseFn,
         scale: ScaleFn,
-        apply_scale: bool = True,
         _backend_fn: Callable = _muygps_diagonal_variance,
     ):
         self._fn = _backend_fn
         self._fn = noise.perturb_fn(self._fn)
-        if apply_scale is True:
-            self._fn = scale.scale_fn(self._fn)
+        self._opt_fn = _reshaper_to_be_removed(self._fn)
+        self._fn = _reshaper_to_be_removed(scale.scale_fn(self._fn))
 
     def __call__(
         self,
@@ -34,13 +34,7 @@ class PosteriorVariance:
         Kcross: mm.ndarray,
         **kwargs,
     ) -> mm.ndarray:
-        if len(Kcross.shape) == 2:
-            batch_count, nn_count = Kcross.shape
-            Kcross = Kcross.reshape(batch_count, 1, nn_count)
-        variances = self._fn(Kin, Kcross, **kwargs)
-        if len(variances.shape) == 1:
-            variances = variances.reshape(variances.shape[0], 1)
-        return variances
+        return self._fn(Kin, Kcross, **kwargs)
 
     def get_opt_fn(self) -> Callable:
-        return self.__call__
+        return self._opt_fn
