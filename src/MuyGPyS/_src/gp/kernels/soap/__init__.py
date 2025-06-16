be1fa613c372873b6812109c4b55fd3b75cec590
gsallabe <gregorysallaberry@gmail.com>
2025-06-16 15:37:51 -0700

    placeholder functions for soap kernel

---
+++ src/MuyGPyS/_src/gp/kernels/soap/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2021-2024 Lawrence Livermore National Security, LLC and other
+# MuyGPyS Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
+
+from MuyGPyS._src.util import _collect_implementation
+
+_soap_fn = _collect_implementation(
+    "_soap_fn",
+)
---
+++ src/MuyGPyS/_src/gp/kernels/soap/numpy.py
@@ -0,0 +1,10 @@
+# Copyright 2021-2024 Lawrence Livermore National Security, LLC and other
+# MuyGPyS Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
+
+import MuyGPyS._src.math.numpy as np
+
+
+def _soap_fn(diffs, **kwargs):
+    return None
